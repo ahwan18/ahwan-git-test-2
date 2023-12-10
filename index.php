7cc6940b78acf6eea03f976d28ae26bea620d4e3
ahwan18 <ahmadkurniawanibrahim@gmail.com>
2023-12-11 00:32:06 +0800

    Adding paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium adipisci maiores totam molestias perferendis aut. Ratione eaque impedit ut sequi nisi id iure blanditiis voluptatum quidem dicta exercitationem, ipsa molestias.</p>
 </body>
 </html>
